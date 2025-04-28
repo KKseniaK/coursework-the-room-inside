74bf72125300f8de69ac537cbffd2bfab7c73943
Ksenia Krotova <ksenia.krotova2020@gmail.com>
2025-04-29 01:31:38 +0500

    asset(room): add wall sprites and initial room layout

---
+++ coursework-the-room-inside/Assets/Scripts/ScenesMenager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour
+{
+    public static ScenesManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+}
